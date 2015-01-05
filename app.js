45a4e2c6c9def066f4a63b676fb48c98e3c56fa2
angus42 <Matthias.Greim@live.com>
2015-01-06 00:02:46 +0100

    Basic plumbing of angular, model, controller and sortable ui.

---
+++ app.js
@@ -0,0 +1,36 @@
+﻿angular.module('myApp', ['ui.sortable'])
+.controller('Ctrl', function ($scope) {
+    $scope.data = {
+        name: "Unnamed",
+        steps: [
+            {
+                frame: [
+                    ["#f00", "#0f0", "#00f"],
+                    ["#f80", "#0f8", "#08f"],
+                    ["#ff0", "#0ff", "#f0f"]
+                ]
+            },
+            {
+                frame : [
+                    ["#f00", "#0f0", "#00f"],
+                    ["#f80", "#0f8", "#08f"],
+                    ["#ff0", "#0ff", "#f0f"]
+                ]
+            }]
+    };
+
+    $scope.load = function (element) {
+        var reader = new FileReader();
+        reader.onload = function () {
+            $scope.$apply(function ($scope) {
+                $scope.data = JSON.parse(reader.result);
+            });
+        };
+        reader.readAsText(element.files[0]);
+    };
+
+    $scope.save = function () {
+        var blob = new Blob([JSON.stringify($scope.data)], { type: "text/json;charset=utf-8" });
+        saveAs(blob, $scope.data.name + ".dfs");
+    };
+});
