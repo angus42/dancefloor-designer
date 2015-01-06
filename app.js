1b0d889b78faf971dda5ae7db2045fc3d48cbe41
angus42 <Matthias.Greim@live.com>
2015-01-06 22:59:09 +0100

    Export to C works now.

--- app.js
+++ app.js
@@ -54,7 +54,30 @@
     };
 
     $scope.exportToC = function () {
-        alert("Not yet implemented!");
+        var firstFrame = $scope.data.steps[0].frame;
+        var content = "const byte " + $scope.data.name + "[][" + firstFrame.length * firstFrame[0].length  + "][3] = {\r\n";
+        for (i = 0; i < $scope.data.steps.length; i++)
+        {
+            if (i > 0)
+                content += ",\r\n";
+            content += "{";
+            var step = $scope.data.steps[i];
+            for (y = 0; y < step.frame.length; y++) {
+                if (y > 0)
+                    content += ",\r\n";
+                for (x = 0; x < step.frame[y].length; x++) {
+                    if (x > 0)
+                        content += ",";
+                    var rgb = hexToRgb(step.frame[y][x].v);
+                    content += "{ " + rgb.r + ", " + rgb.g + ", " + rgb.b + " }";
+                }
+            }
+            content += "}";
+        }
+        content += "\r\n};";
+
+        var blob = new Blob([content], { type: "text/plain;charset=utf-8" });
+        saveAs(blob, $scope.data.name + ".h");
     };
 
     $scope.selectStep = function (step) {
@@ -129,5 +152,19 @@
             $scope.playAnimation();
         }
     };
-
 }]);
+
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
